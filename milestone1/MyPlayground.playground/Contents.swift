9776e6a09e7211f2763b636f14a94cd50a18784d
Felipe Zadinello <felipe.zadinello@blacklane.com>
2025-03-20 15:35:35 +0100

    [MS-1] Checkpoint 2
    This time the challenge is to create an array of strings, then write some code that prints the number of items in the array and also the number of unique items in the array.

--- milestone1/MyPlayground.playground/Contents.swift
+++ milestone1/MyPlayground.playground/Contents.swift
@@ -7,3 +7,9 @@ let fahrenheit: Double = (celsius * 9.0 / 5.0) + 32.0
 print("\(fahrenheit)°F")
 print("\(celsius)°C")
 
+let albums = ["Thriller", "Back in Black", "Purple Rain", "Thriller"]
+print("Albums size: \(albums.count)")
+
+let setOfAlbums = Set(albums)
+print("Unique albums size: \(setOfAlbums.count)")
+
