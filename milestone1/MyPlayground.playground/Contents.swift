67d548b06d805982c61ef6fcc386972fcc9a6940
Felipe Zadinello <felipe.zadinello@blacklane.com>
2025-03-20 15:08:11 +0100

    [MS-1] Check point 1
    Variables, constants, strings, and numbers
    Booleans, string interpolation

--- milestone1/MyPlayground.playground/Contents.swift
+++ milestone1/MyPlayground.playground/Contents.swift
@@ -1,3 +1,9 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let celsius: Double = 25.5
+
+let fahrenheit: Double = (celsius * 9.0 / 5.0) + 32.0
+
+print("\(fahrenheit)°F")
+print("\(celsius)°C")
+
