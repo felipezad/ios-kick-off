4aff9d36cd1cac1f673094e9e79839520fac8215
Felipe Zadinello <felipe.zadinello@blacklane.com>
2025-03-20 16:30:46 +0100

    [MS-1] Checkpoint 4

--- milestone1/MyPlayground.playground/Contents.swift
+++ milestone1/MyPlayground.playground/Contents.swift
@@ -14,15 +14,36 @@ let setOfAlbums = Set(albums)
 print("Unique albums size: \(setOfAlbums.count)")
 
 
-for number in 1...100 {
-  if(number.isMultiple(of: 3) && number.isMultiple(of: 5)){
-    print("FizzBuzz -> number: \(number)\n")
-  }else if(number.isMultiple(of: 3)){
-    print("Fizz -> number: \(number)\n")
-  }else if (number.isMultiple(of: 5)){
-    print("Buzz -> number: \(number)\n")
-  } else {
-     print("Number: \(number)\n")
+//for number in 1...100 {
+//  if(number.isMultiple(of: 3) && number.isMultiple(of: 5)){
+//    print("FizzBuzz -> number: \(number)\n")
+//  }else if(number.isMultiple(of: 3)){
+//    print("Fizz -> number: \(number)\n")
+//  }else if (number.isMultiple(of: 5)){
+//    print("Buzz -> number: \(number)\n")
+//  } else {
+//     print("Number: \(number)\n")
+//  }
+//}
+
+enum CustomError : Error {
+  case OutOfBoundsExcpetion
+  case NotInteger
+}
+func findSqrtRoot(_ number: Int){
+  do {
+    if(number < 1 || number > 10000){
+      throw CustomError.OutOfBoundsExcpetion
+    }
+    let result = sqrt(Double(number))
+    if(result.truncatingRemainder(dividingBy: 1) != 0){
+      throw CustomError.NotInteger
+    }
+    
+    print("Sqrt root of \(number) is: \(result)")
+  } catch {
+    print("Error: \(error)")
   }
 }
 
+findSqrtRoot(1000)
