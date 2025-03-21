e2461217891906ca018b8b4542adc797e923154b
Felipe Zadinello <felipe.zadinello@blacklane.com>
2025-03-21 16:21:42 +0100

    [MS-1] Checkpoint 6

--- milestone1/MyPlayground.playground/Contents.swift
+++ milestone1/MyPlayground.playground/Contents.swift
@@ -53,3 +53,33 @@ let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
 
 
 print(luckyNumbers.filter { $0 % 2 != 0}.sorted().map { "\($0) is a lucky number!"})
+
+
+struct Car {
+  let model: String
+  let numberOfSeats: Int
+  private var currentGear: Int {
+    didSet {
+      print("The gear now is \(currentGear)")
+    }
+    willSet {
+      print("Current gear is \(currentGear)")
+      print("The gear will be \(newValue)")
+    }
+  }
+  
+  
+  init(model: String, numberOfSeats: Int, currentGear: Int) {
+    self.model = model
+    self.numberOfSeats = numberOfSeats
+    self.currentGear = currentGear
+  }
+  
+  mutating func changeGear(_ newGear: Int) {
+    currentGear = newGear
+  }
+}
+
+var car = Car(model: "XYZ", numberOfSeats: 6, currentGear: 1)
+car.changeGear(2)
+
